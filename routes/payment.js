e0b53664da2041fbf1c871b62d09f6a57206c55b
jkarelins <jkarelins@gmail.com>
2020-03-25 19:19:49 +0100

    basic payment routes using stripe

--- routes/payment.js
+++ routes/payment.js
@@ -1,31 +1,28 @@
-const { createMollieClient } = require("@mollie/api-client");
 const { Router } = require("express");
 const router = new Router();
 require("dotenv").config();
 
-// const mollieClient = createMollieClient({
-//   apiKey: process.env.MOLLIE_TEST_KEY
-// });
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
-// router.get("/", (req, res, next) => {
-//   mollieClient.payments
-//     .create({
-//       amount: {
-//         value: "10.00",
-//         currency: "EUR"
-//       },
-//       description: "My first API payment",
-//       redirectUrl: "https://yourwebshop.example.org/order/123456",
-//       webhookUrl: "https://yourwebshop.example.org/webhook"
-//     })
-//     .then(payment => {
-//       console.log(payment.getCheckoutUrl());
-//       // Forward the customer to the payment.getCheckoutUrl()
-//     })
-//     .catch(error => {
-//       console.log(error);
-//       // Handle the error
-//     });
-// });
+router.get("/:amountInCents", async (req, res, next) => {
+  const { amountInCents } = req.params;
+  stripe.paymentIntents
+    .create({
+      amount: amountInCents,
+      currency: "eur",
+      // customer: 1,
+      metadata: { integration_check: "accept_a_payment" }
+    })
+    .then(paymentIntent => {
+      //create and send intent
+      res.send(paymentIntent);
+    })
+    .catch(next);
+});
+
+router.post("/confirmed", (req, res, next) => {
+  console.log("good");
+  res.send({ confirmed: true });
+});
 
 module.exports = router;
